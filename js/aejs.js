6932a64465db453406b825e01f21ca683a9a7873
Nathaniel Smith <ntotheg45@gmail.com>
2020-05-18 16:51:51 -0400

    see master branch for update notes.

--- js/aejs.js
+++ js/aejs.js
@@ -4,6 +4,7 @@ $('.jsDisabled').hide();
 //SHOW BIOBUTTON
 $('.bioButton').show();
 
+//--------------------------------------------------------------RADIO-----------------------------------------------------------------
 //ESTABLISH MUSIC OFF WITH VARIABLE SET TO FALSE
 var currentPlay = false; 
 
@@ -47,3 +48,37 @@ $('.On').on('click', function() {
     $(this).prev().prev().trigger('pause');
     $(this).prev().show();
 });
+
+//FUNCTION TO DISPLAY HINT IF USER DOES NOT CLICK ON SONGBUTTONS AND TIMER WILL RESET IF USER ONLY CLICKS ON BIO
+$(document).ready(function() {
+    //SET TIMER VALUE
+    let timer = 0;
+    //SET HINT VALUE
+    let hint = true;
+    //CREATE COUNTER
+    let seconds = setInterval(stopWatch, 1000);
+    //SET HINT TO FALSE IF USER CLICKS ON SONGBUTTONS
+    $('.songButton').on('click', function() {
+      hint = false;
+    });
+    //RESET TIME IF MOUSE ENTERS SONGBUTTONS OR SOUNDCLOUDBUTTON
+    $('.songButton').on('mouseenter mouseleave', function() {
+       timer = 0; 
+    });
+    $('.bioButton').on('mouseenter', function() {
+        timer = 0;
+    });
+    //RESET TIME IF USER CLICKS OR MOUSEENTERS BIOBUTTON
+    $('.bioButton').on('click', function() {
+        timer = -15;
+    });
+    //  CREATE STOPWATCH FUNCTION TO DISPLAY HINT IF TIMER REACHES 15 SECONDS AND HINT IS TRUE
+    function stopWatch() {
+        timer++;
+        if (timer == 15 && hint == true) {
+            $('.hints').fadeIn(2000).delay(5000).fadeOut(2000);
+        }
+    }
+});
+
+
